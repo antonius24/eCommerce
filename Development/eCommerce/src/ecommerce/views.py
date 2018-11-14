d46abcefb312c2846969f4e87765ad3679642134
Xiao Liu <liuxiao.qmbupt@gmail.com>
2018-11-13 17:33:40 -0800

    Add contact page and about page

--- Development/eCommerce/src/ecommerce/views.py
+++ Development/eCommerce/src/ecommerce/views.py
@@ -30,4 +30,22 @@ def home_page_old(request):
 	return HttpResponse(html_)
 
 def home_page(request):
-	return render(request, "home_page.html", {})
+	context = {
+		"title":"Hello World!",
+		"content":" Welcome to the homepage."
+	}
+	return render(request, "home_page.html", context)
+
+def about_page(request):
+	context = {
+		"title":"about page",
+		"content":" Welcome to the about page."
+	}
+	return render(request, "home_page.html", context)
+
+def contact_page(request):
+	context = {
+		"title":"contact page",
+		"content":" Welcome to the contact page."
+	}
+	return render(request, "home_page.html", context)
