5829f2238ccaffdffa300fcf9cbdd4998bd8bcce
Xiao Liu <liuxiao.qmbupt@gmail.com>
2018-11-16 22:37:43 -0800

    Using Django form instead of hard coded html form. Add customized email data clean logic

--- Development/eCommerce/src/ecommerce/views.py
+++ Development/eCommerce/src/ecommerce/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
- 
+from .forms import ContactForm
 def home_page_old(request):
 	html_ = """
 	<!doctype html>
@@ -44,12 +44,16 @@ def about_page(request):
 	return render(request, "home_page.html", context)
 
 def contact_page(request):
+	contact_form = ContactForm(request.POST or None)
 	context = {
 		"title":"contact page",
-		"content":" Welcome to the contact page."
+		"content":" Welcome to the contact page.",
+		"form": contact_form
 	}
-	if request.method == "POST":
-		print(request.POST.get('fullname'))
-		print(request.POST.get('email'))
-		print(request.POST.get('content'))
+	if contact_form.is_valid():
+		print(contact_form.cleaned_data)
+	# if request.method == "POST":
+	# 	print(request.POST.get('fullname'))
+	# 	print(request.POST.get('email'))
+	# 	print(request.POST.get('content'))
 	return render(request, "contact/view.html", context)
