0a3463526896b40e348085ac14fa2cce6d79a352
Xiao Liu <liuxiao.qmbupt@gmail.com>
2018-11-26 15:23:34 -0800

    Create login page, user Django buildin to manage login logic

--- Development/eCommerce/src/ecommerce/views.py
+++ Development/eCommerce/src/ecommerce/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from django.shortcuts import render
-from .forms import ContactForm
+from django.shortcuts import render, redirect
+from .forms import ContactForm, LoginForm
 def home_page_old(request):
 	html_ = """
 	<!doctype html>
@@ -57,3 +58,32 @@ def contact_page(request):
 	# 	print(request.POST.get('email'))
 	# 	print(request.POST.get('content'))
 	return render(request, "contact/view.html", context)
+
+def login_page(request):
+	form = LoginForm(request.POST or None)
+	context = {
+		"form": form
+	}
+	print("User logged in")
+	#print(request.user.is_authenticated())
+	if form.is_valid():
+		print(form.cleaned_data)
+		username = form.cleaned_data.get("username")
+		password = form.cleaned_data.get("password")
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			#print(request.user.is_authenticated())
+			login(request, user)
+        	# Redirect to a success page.
+			return redirect("/login")
+		else:
+			# Return an 'invalid login' error message.
+			print("error")
+
+	return render(request, "auth/login.html", context)
+
+def register_page(request):
+	form = LoginForm(request.POST or None)
+	if form.is_valid():
+		print(form.cleaned_data)
+	return render(request, "auth/register.html", {})
