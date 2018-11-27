70434fa13496f03fbeac3bf4c1137adc8e4fcbaf
Xiao Liu <liuxiao.qmbupt@gmail.com>
2018-11-27 14:57:05 -0800

    Update settings.py to setup local separate static server, this is to emulate the static server separated from django server. This is for local develpment test. When go live we will need to setup the real static server.

--- Development/eCommerce/src/ecommerce/settings.py
+++ Development/eCommerce/src/ecommerce/settings.py
@@ -117,4 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# The below settings is only for local setup. You will have different settings when you go live
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_my_proj"),
+]
+# Emulate a static server which is separate from our django server
+STATIC_ROOT =  os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT =  os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
