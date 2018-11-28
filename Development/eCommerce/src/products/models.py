5bffbae6540fceb3a2f044bd36249c8f46a35fc1
Xiao Liu <liuxiao.qmbupt@gmail.com>
2018-11-27 16:02:41 -0800

    Create first model for products app

--- Development/eCommerce/src/products/models.py
+++ Development/eCommerce/src/products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+# Model Field Reference: https://docs.djangoproject.com/en/2.1/ref/models/fields/
+class Product(models.Model):
+	title = models.CharField(max_length=120)
+	description = models.TextField()
+	price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
